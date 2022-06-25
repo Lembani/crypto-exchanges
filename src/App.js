b0a55050ab9566609be36597a60c8d094b8dc844
Lembani Sakala <lembanisakala@gmail.com>
2022-06-25 12:21:50 +0200

    Add Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React from 'react';
+import {
+  Routes,
+  Route,
+} from 'react-router-dom';
+
+import Markets from './components/Markets';
+
 function App() {
   return (
     <div className="app">
       <h1>Crypto Exchanges</h1>
+
+      <Routes>
+        <Route path="/" element={<Markets />} />
+      </Routes>
     </div>
   );
 }
